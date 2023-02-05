9dd090bcd29ba0f7118d601b0d35a2f166b0241b
n1c00o <n@nc0.fr>
2023-02-05 11:29:53 +0100

    Deleting current code

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .compile(
-                &["./proto/n1c00o/clawflake/v2/clawflake.proto"],
-                &["./proto", "./third_party/googleapis"]
-        )
-        .unwrap();
-    Ok(())
-}
